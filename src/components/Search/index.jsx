b03a1c92bfb3505e4c0a16c57b2bf5679fc90dd3
Black Bobs <ayodejiayodele350@gmail.com>
2024-07-01 22:53:43 +0100

    feat:daily forecast

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 
 const Search = () => {
   return (
-    <div className='flex gap-2 items-center rounded-full p-2 bg-slate-200'>
+    <div className='flex gap-2 items-center rounded-full p-2 bg-slate-200 shadow-md'>
         <CiSearch/>
         <input className='bg-transparent focus:outline-none' type="text" placeholder='Search for your city' />
     </div>
